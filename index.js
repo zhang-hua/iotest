59eec5cb0e6b48407535d9578f457c36caba22c5
Zhang Hua <zhang-hua@users.noreply.github.com>
2019-06-11 16:52:18 -0700

    change port to 80

--- index.js
+++ index.js
@@ -66,6 +66,6 @@ app.use(function(req, res, next) {
 });
 
 // start the server in the port 3000 !
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000.');
+app.listen(80, function () {
+    console.log('Example app listening on port 80.');
 });
